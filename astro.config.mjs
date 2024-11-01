24c6dc64b0d5ea40daae63a9a5a17f6723eb5deb
John Berroa <jhonberroa@hotmail.es>
2024-11-01 13:21:29 -0400

    deploy github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://johnb03.github.io',
-  base: 'portafolio-John-Berroa',
+  base: 'my-repo',
 });
